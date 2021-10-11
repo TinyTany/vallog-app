7cd837051e10b3627e162c7ecd7f30ce20843fbd
TinyTany <t4nishi.x@gmail.com>
2021-10-11 18:33:24 +0900

    nodeモジュールをブラウザ側で使用可能にする

---
+++ public/scripts/require.js
@@ -0,0 +1,10 @@
+(() => {
+    window.modules = {};
+    let mod = window.modules;
+    mod.babel_parser = require('@babel/parser');
+    mod.babel_traverse = require('@babel/traverse');
+    mod.babel_generator = require('@babel/generator');
+    mod.babel_template = require('@babel/template');
+    mod.babel_types = require('@babel/types');
+    mod.sandbox = require('sandbox');
+})();
